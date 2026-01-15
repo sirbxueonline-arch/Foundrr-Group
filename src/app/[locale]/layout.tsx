913c87187dce7f073e5b97edc19a6bb06db98acf
sirbxueonline-arch <sirbxueonline@gmail.com>
2026-01-15 15:16:00 +0300

    d

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,11 +1,12 @@
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages} from 'next-intl/server';
-import {Inter} from 'next/font/google';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { Inter } from 'next/font/google';
 import '../globals.css';
 import { Metadata } from 'next';
 import Navbar from '@/components/Navbar';
+import { Analytics } from "@vercel/analytics/react"
 
-const inter = Inter({subsets: ['latin']});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Foundrr Group',
@@ -17,24 +18,25 @@ export default async function LocaleLayout({
   params
 }: {
   children: React.ReactNode;
-  params: Promise<{locale: string}>;
+  params: Promise<{ locale: string }>;
 }) {
   // Ensure that the incoming `locale` is valid
-  const {locale} = await params;
- 
+  const { locale } = await params;
+
   // Providing all messages to the client
   // side is the easiest way to get started
   const messages = await getMessages();
- 
+
   // Determine text direction (optional, for AZ/EN both are LTR but good practice)
   const dir = 'ltr';
- 
+
   return (
     <html lang={locale} dir={dir}>
       <body className={inter.className}>
         <NextIntlClientProvider messages={messages}>
           <Navbar />
           {children}
+          <Analytics />
         </NextIntlClientProvider>
       </body>
     </html>
